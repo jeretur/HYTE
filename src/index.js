1fdf8a8b4fc2274ee336a15aca4cdcfe424a10de
Jere Turunen <jere1.turunen@gmail.com>
2026-01-19 15:09:05 +0200

    refactor: move items data & functions to separate module

--- src/index.js
+++ src/index.js
@@ -1,80 +1,41 @@
 import express from 'express';
+import { getItems, putItemById, getItemById, deleteItemById, postItem } from './items.js';
 const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
 
-// Dummy data (nollataan aina kun sovelluksen käynnistää uudelleen)
-const items = [
-  {id: 1, name: 'Maitorahka'},
-  {id: 2, name: 'Banaani'},
-  {id: 3, name: 'Hunaja'},
-];
 
 // parsitaan json data request ja lisää request-objektiin
 app.use(express.json());
+
+//Tarjoillaan webbisivut (frontend) palvelimen juuressa
+
+app.use(express.static('public'));
+
 
 // API root
 app.get('/', (req, res) => {
   res.send('Welcome to my REST API!');
 });
 
+// Endpoints for items resource
 // Get all items
-app.get('/items', (req, res) => {
-  res.json(items);
-});
+app.get('/items', getItems);
 
 // Get items based on id
-app.get('/items/:id', (req, res) => {
-  console.log('get item with id', req.params.id);
-  const itemFound = items.find((item) => item.id == req.params.id);
-  res.json(itemFound);
-  if (itemFound) {
-    res.json(itemFound);
-  } else {
-    res.status(404).json({message: 'Item not found'});
-  }
-});
+app.get('/items/:id', getItemById);
 
 // TOOD: add PUT route
 
-app.put('/items/:id', (req, res) => {
-  console.log('update item with id', req.params.id);
-  const itemIndex = items.findIndex((item) => item.id == req.params.id);
-  if (itemIndex !== -1) {
-    items[itemIndex] = { id: parseInt(req.params.id), ...req.body };
-    res.json({ message: 'Item updated', item: items[itemIndex] });
-  } else {
-    res.status(404).json({ message: 'Item not found' });
-  }
-} );
+app.put('/items/:id', putItemById );
 
 // TODO add DELETE route
-app.delete('/items/:id', (req, res) => {
-  console.log('delete item with id', req.params.id);
-  const itemIndex = items.findIndex((item) => item.id == req.params.id);
-  if (itemIndex !== -1) {
-    items.splice(itemIndex, 1);
-    res.json({ message: 'Item deleted' });
-  } else {
-    res.status(404).json({ message: 'Item not found' });
-  }
-} );
+app.delete('/items/:id', deleteItemById);
 
 
 // Add new item
-app.post('/items', (req, res) => {
-  //console.log('add item request body', req.body);
-  //TODO: lisää id listaan ja tarkista että body sisältää name kentän
-  const newId = items.length > 0 ? items[items.length - 1].id + 1 : 1;
-  req.body.id = newId;
-  if (req.body.name) {
-    items.push(req.body);
-    res.status(201).json({message: 'Item added', item: req.body});
-  } else {
-    res.status(400).json({message: 'Invalid item data'});
-  }
-});
+app.post('/items', postItem );
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ src/items.js
@@ -0,0 +1,58 @@
+// Dummy data (nollataan aina kun sovelluksen käynnistää uudelleen)
+const items = [
+  {id: 1, name: 'Maitorahka'},
+  {id: 2, name: 'Banaani'},
+  {id: 3, name: 'Hunaja'},
+];
+
+const getItems = (req, res) => {
+  res.json(items);
+};
+
+const getItemById = (req, res) => {
+  console.log('get item with id', req.params.id);
+  const itemFound = items.find((item) => item.id == req.params.id);
+  res.json(itemFound);
+  if (itemFound) {
+    res.json(itemFound);
+  } else {
+    res.status(404).json({message: 'Item not found'});
+  }
+}
+
+const putItemById = (req, res) => {
+  console.log('update item with id', req.params.id);
+  const itemIndex = items.findIndex((item) => item.id == req.params.id);
+  if (itemIndex !== -1) {
+    items[itemIndex] = { id: parseInt(req.params.id), ...req.body };
+    res.json({ message: 'Item updated', item: items[itemIndex] });
+  } else {
+    res.status(404).json({ message: 'Item not found' });
+  }
+};
+
+const deleteItemById = (req, res) => {
+  console.log('delete item with id', req.params.id);
+  const itemIndex = items.findIndex((item) => item.id == req.params.id);
+  if (itemIndex !== -1) {
+    items.splice(itemIndex, 1);
+    res.json({ message: 'Item deleted' });
+  } else {
+    res.status(404).json({ message: 'Item not found' });
+  }
+};
+
+const postItem = (req, res) => {
+  //console.log('add item request body', req.body);
+  //TODO: lisää id listaan ja tarkista että body sisältää name kentän
+  const newId = items.length > 0 ? items[items.length - 1].id + 1 : 1;
+  req.body.id = newId;
+  if (req.body.name) {
+    items.push(req.body);
+    res.status(201).json({message: 'Item added', item: req.body});
+  } else {
+    res.status(400).json({message: 'Invalid item data'});
+  }
+};
+
+export { getItems, getItemById, putItemById, deleteItemById, postItem };
