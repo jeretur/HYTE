0692e1bfe6331f013444e82ac7a90a2a617ec342
Jere Turunen <jere1.turunen@gmail.com>
2026-01-19 13:07:54 +0200

    Last TODO added

--- src/index.js
+++ src/index.js
@@ -41,14 +41,13 @@ app.get('/items/:id', (req, res) => {
 app.put('/items/:id', (req, res) => {
   console.log('update item with id', req.params.id);
   const itemIndex = items.findIndex((item) => item.id == req.params.id);
-  res.json(itemIndex);
   if (itemIndex !== -1) {
-    items[itemIndex] = { id: items[itemIndex].id, ...req.body };
+    items[itemIndex] = { id: parseInt(req.params.id), ...req.body };
     res.json({ message: 'Item updated', item: items[itemIndex] });
   } else {
     res.status(404).json({ message: 'Item not found' });
   }
-});
+} );
 
 // TODO add DELETE route
 app.delete('/items/:id', (req, res) => {
@@ -60,15 +59,21 @@ app.delete('/items/:id', (req, res) => {
   } else {
     res.status(404).json({ message: 'Item not found' });
   }
-});
+} );
 
 
 // Add new item
 app.post('/items', (req, res) => {
   //console.log('add item request body', req.body);
   //TODO: lisää id listaan ja tarkista että body sisältää name kentän
-  items.push(req.body);
-  res.status(201).json({message: 'uusi itemi lisätty.'});
+  const newId = items.length > 0 ? items[items.length - 1].id + 1 : 1;
+  req.body.id = newId;
+  if (req.body.name) {
+    items.push(req.body);
+    res.status(201).json({message: 'Item added', item: req.body});
+  } else {
+    res.status(400).json({message: 'Invalid item data'});
+  }
 });
 
 app.listen(port, hostname, () => {
