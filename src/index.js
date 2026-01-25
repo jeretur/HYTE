49230ead21fd66cdc40388666c22f1b6bda2e7c8
Jere Turunen <jere1.turunen@gmail.com>
2026-01-25 12:57:10 +0200

    TODOS Done

--- src/index.js
+++ src/index.js
@@ -1,41 +1,57 @@
 import express from 'express';
-import { getItems, putItemById, getItemById, deleteItemById, postItem } from './items.js';
+import {
+  deleteItemById,
+  getItemById,
+  getItems,
+  postItem,
+  putItemById,
+} from './items.js';
+import {getUsers, postLogin, postUser, getUserById, putUserById, deleteUserById} from './users.js';
 const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
-
-
-// parsitaan json data request ja lisää request-objektiin
+// parsitaan json data pyynnöstä ja lisätään request-objektiin
 app.use(express.json());
 
-//Tarjoillaan webbisivut (frontend) palvelimen juuressa
-
-app.use(express.static('public'));
-
+// tarjoillaan webbisivusto (front-end) palvelimen juuressa
+app.use('/', express.static('public'));
 
 // API root
-app.get('/', (req, res) => {
-  res.send('Welcome to my REST API!');
+app.get('/api', (req, res) => {
+  res.send('This is dummy items API!');
 });
 
-// Endpoints for items resource
+// Endpoints for 'items' resource
 // Get all items
 app.get('/api/items', getItems);
-
-// Get items based on id
+// Get item based on id
 app.get('/api/items/:id', getItemById);
-
-// TOOD: add PUT route
-
-app.put('/api/items/:id', putItemById );
-
-// TODO add DELETE route
+// PUT route for items
+app.put('/api/items/:id', putItemById);
+// DELETE route for items
 app.delete('/api/items/:id', deleteItemById);
-
-
 // Add new item
-app.post('/api/items', postItem );
+app.post('/api/items', postItem);
+
+// Users resource endpoints
+// GET all users
+app.get('/api/users', getUsers);
+// POST new user
+app.post('/api/users', postUser);
+// POST user login
+app.post('/api/users/login', postLogin);
+
+// TODO: get user by id
+app.get('/api/users/:id', getUserById);
+// app.get('/api/users/:id');
+
+// TODO: put user by id
+app.put('/api/users/:id', putUserById);
+
+
+// TODO: delete user by id
+app.delete('/api/users/:id', deleteUserById);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
